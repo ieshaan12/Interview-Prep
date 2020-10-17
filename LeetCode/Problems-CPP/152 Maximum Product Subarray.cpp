c879f92dc3c372b39f7acaf3cfd8ca7870ca7ec2
Nitin Vinayak Agrawal <nvagrawal2701@gmail.com>
2020-10-17 21:14:01 +0530

    Create 152 Maximum Product Subarray.cpp

---
+++ LeetCode/Problems-CPP/152 Maximum Product Subarray.cpp
@@ -0,0 +1,26 @@
+#Time Taken(%):20 ms
+#Memory usage(%):12 MB
+#Your Name(optional):nitinvinayak
+#// Your solution here
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size(),i,res,minPrev,maxPrev,minCurr,maxCurr;
+        
+        maxPrev=nums[0];
+        minPrev=nums[0];
+        
+        res=maxPrev;
+        
+        for(i=1;i<n;i++){
+            maxCurr=max(nums[i],max(maxPrev*nums[i],minPrev*nums[i]));
+            minCurr=min(nums[i],min(maxPrev*nums[i],minPrev*nums[i]));
+            
+            res=max(res,maxCurr);
+            
+            maxPrev=maxCurr;
+            minPrev=minCurr;
+        }
+        return res;
+    }
+};
