8292d9c6e393ec58d5bf832766680a248c34d6ad
Nitin Vinayak Agrawal <nvagrawal2701@gmail.com>
2020-10-17 21:32:35 +0530

    Create 32 Longest Valid Parentheses.cpp

---
+++ LeetCode/Problems-CPP/32 Longest Valid Parentheses.cpp
@@ -0,0 +1,33 @@
+#Time Taken(%):4 ms
+#Memory usage(%):7.6 MB
+#Your Name(optional):nitinvinayak
+#// Your solution here
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n=s.size(),i,L,res;
+        if(n==0) return 0;
+        vector<int> dp(n,0);
+        
+        dp[0]=0;
+        res=dp[0];
+        for (i=1;i<n;i++){
+            if(s[i]=='(') dp[i]=0;
+            
+            else{
+                L=dp[i-1];
+                if(i-L-1<0 || s[i-L-1]==')')
+                    dp[i]=0;
+                else{
+                    dp[i]=2+dp[i-1];
+                    
+                    if(i-L-2>=0)
+                        dp[i]+=dp[i-L-2];
+                }
+            }
+                res=max(res,dp[i]);
+        }
+        return res;
+    }
+    
+};
